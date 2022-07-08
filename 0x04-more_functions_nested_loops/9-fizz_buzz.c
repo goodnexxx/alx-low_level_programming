f65e654dc1caac091e64620b6f11366b12b6992d
goodness <goodnessadaokun@gmail.com>
2022-07-08 15:19:25 -0700

    g

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 		else if ((i % 5) == 0 && (i % 3) != 0 && i != 100)
 			printf("Buzz ");
 		else if ((i % 5) == 0 && (i % 3) != 0 && i == 100)
-			printf("Buzz ");
+			printf("Buzz");
 		else if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz ");
 		else
