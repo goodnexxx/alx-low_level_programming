144f909a0b6346ec849bfb09930c39233a1cb11b
goodness <goodnessadaokun@gmail.com>
2022-07-14 15:05:40 -0700

    F

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,30 +2,22 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes the first letter of each word
+ * cap_string - capitalizes the first letter of  word
  * @s: a pointer to the string
- * Return: returns s
+ * Return: returns *s
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char k[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; s[i] != 0; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i + 1] >= 97 && s[i + 1] <= 122)
-		{
-			if (s[i] == ' ' || s[i] == '\n' || s[i] == ','
-				|| s[i] == ';' || s[i] == '.' || s[i] == '!'
-					|| s[i] == '?' || s[i] == '"' || s[i] == '('
-						|| s[i] == ')' || s[i] == '{' || s[i] == '}')
-			{
-				s[i + 1] = s[i + 1] - 32;
-			}
-			if (s[i] == '\t')
-			{
-				s[i + 1] = s[i + 1] - 32;
-			}
-		}
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+				for (j = 0; k[j] != '\0'; j++)
+					if (s[i] == k[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
