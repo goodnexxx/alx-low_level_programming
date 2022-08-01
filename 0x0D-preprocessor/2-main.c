45f27ad173a3c48e15b2dd3a4b1bbadb0eeb69ad
goodness <goodnessadaokun@gmail.com>
2022-08-01 23:00:07 +0000

    F

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the file it was compiled from
+ * Return: return 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
