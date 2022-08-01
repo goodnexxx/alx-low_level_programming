29cafa0e46077ffa91456459935dbf65a848d34a
goodness <goodnessadaokun@gmail.com>
2022-08-01 23:16:22 +0000

    F

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -10,9 +10,9 @@
  * @owner: a string 'owner'
  * Return: returns nothing
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
